84285488fbc76505eacb129667d26b5dff86a9c5
ivo lejon <ivo.lejon@gmail.com>
2015-11-03 21:29:46 +0100

    Version 1.1 med mouseover

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,26 @@ var notify = require('gulp-notify');
 var livereload = require('gulp-livereload');
 
 var concat = require('gulp-concat');
-var minify = require('gulp-minify');
+var minifyJs = require('gulp-minify');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
+var url_adujuster = require('gulp-css-url-adjuster')
+var replace = require('gulp-replace-task');
 
-var vendor = ['public/vendor/bootstrap/']
+gulp.task('minify-js', function() {
+  return gulp.src(['./public/js/home.js'])
+    .pipe(concat('site.js'))
+    .pipe(minifyJs())
+    .pipe(gulp.dest('./public/dist/js'));
+});
+
 
 gulp.task('minify-css', function() {
-  return gulp.src('public/css/matpriser.css')
+     gulp.src('./public/css/matpriser.css')
+    .pipe(url_adujuster({
+    replace:  ['../img/','../image/'],
+  	}))
   	.pipe(autoprefixer())
     .pipe(minifyCss({compatibility: 'ie8','processImport':false}))
     .pipe(rename({suffix: '.min'}))
@@ -21,7 +32,34 @@ gulp.task('minify-css', function() {
     
 });
 
-gulp.task('default', [], function() {
+gulp.task('copy-images', function() {
+   gulp.src(['./public/image/*'])
+   .pipe(gulp.dest('./public/dist/image'));
+});
+
+
+gulp.task('fix-urls', function() {
+   gulp.src(['./views/*.handlebars','./views/layouts/*.handlebars'])
+   .pipe(replace({
+   	usePrefix:false,
+      patterns: [
+        {
+          match: 'image/',
+          replacement: 'dist/image/'
+        }
+      ]
+    }))
+    .pipe(gulp.dest(function(file) {
+    return file.base;
+  }));
+});
+
+
+gulp.task('default', ['minify-css','copy-images','minify-js'], function() {
+
+});
+
+gulp.task('run', function() {
 	livereload.listen();
 	nodemon({
 		// the script to run the app
