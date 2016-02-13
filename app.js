02bc0718fc88c4c968fad6994d0f758f1f658e26
ivo lejon <ivo.lejon@gmail.com>
2016-02-13 20:00:38 +0100

    backend

--- app.js
+++ app.js
@@ -37,7 +37,11 @@ app.use(fetch);
 app.use(home);
 app.use(admin);
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(function(err, req, res, next) {
   console.error(err.stack);
